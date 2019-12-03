52878a8a757b033f2851619e1c892447db46e23d
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2019-12-03 12:03:21 +0300

    Add validated triage query

--- IQCare_ValidatedQueries.sql
+++ IQCare_ValidatedQueries.sql
@@ -71,3 +71,99 @@ FROM Person P
 INNER JOIN Patient PT ON PT.PersonId = P.Id
 ORDER BY P.Id ASC
 
+
+--2. HIV ENROLLMENT. Careful thought on this on
+/*SELECT
+PT.Id PatientId,
+PT.PersonId Person_Id,
+Patient_Type = (SELECT ItemName FROM LookupItemView LK WHERE LK.ItemId = PT.PatientType AND LK.MasterName = 'PatientType'),
+Entry_point = (SELECT TOP 1 LK.ItemName FROM ServiceEntryPoint SE INNER JOIN LookupItemView LK ON LK.ItemId = EntryPointId WHERE SE.PatientId = PT.Id AND ServiceAreaId = 1 ORDER BY SE.Id DESC),
+TI_Facility = (select FacilityFrom from PatientTransferIn PTI WHERE PTI.PatientId = PT.Id),
+CASE (SELECT ItemName FROM LookupItemView LK WHERE LK.ItemId = PT.PatientType AND LK.MasterName = 'PatientType')
+WHEN 'New' THEN PE.EnrollmentDate
+WHEN 'Transit' THEN PE.EnrollmentDate
+WHEN 'Transfer-In' THEN (SELECT TOP 1 EnrollmentDate from PatientHivDiagnosis PHD WHERE PHD.PatientId = PT.Id AND PHD.DeleteFlag = 0 ORDER BY Id DESC)
+ELSE PE.EnrollmentDate END AS Date_First_enrolled_in_care,
+CASE (SELECT ItemName FROM LookupItemView LK WHERE LK.ItemId = PT.PatientType AND LK.MasterName = 'PatientType')
+WHEN 'New' THEN NULL
+WHEN 'Transit' THEN NULL
+WHEN 'Transfer-In' THEN (select TransferInDate from PatientTransferIn PTI WHERE PTI.PatientId = PT.Id AND PTI.DeleteFlag = 0)
+ELSE NULL END AS Transfer_In_Date,
+CASE (SELECT ItemName FROM LookupItemView LK WHERE LK.ItemId = PT.PatientType AND LK.MasterName = 'PatientType')
+WHEN 'New' THEN NULL
+WHEN 'Transit' THEN NULL
+WHEN 'Transfer-In' THEN (select TreatmentStartDate from PatientTransferIn PTI WHERE PTI.PatientId = PT.Id AND PTI.DeleteFlag = 0)
+ELSE NULL END AS Date_started_art_at_transferring_facility,
+
+
+PT.DeleteFlag Voided
+
+
+FROM Patient PT
+INNER JOIN PatientEnrollment PE ON PE.PatientId = PT.Id
+WHERE PE.ServiceAreaId = 1;*/
+
+--3 Triage
+SELECT 
+P.PersonId Person_Id,
+P.Id Patient_Id,
+Encounter_Date = format(cast(PM.VisitDate as date),'yyyy-MM-dd'),
+Encounter_ID = PM.Id,
+Visit_reason = NULL,
+Systolic_pressure = CAST(PV.BPSystolic AS decimal),
+Diastolic_pressure = CAST(PV.BPDiastolic AS decimal),
+Respiratory_rate = PV.RespiratoryRate,
+Pulse_rate = PV.HeartRate,
+Oxygen_saturation = PV.SpO2,
+Weight = PV.Weight,
+Height = PV.Height,
+Temperature = PV.Temperature,
+BMI = PV.BMI,
+Muac = PV.Muac,
+Weight_for_age_zscore = PV.WeightForAge,
+Weight_for_height_zscore = PV.WeightForHeight,
+BMI_Zscore = PV.BMIZ,
+Head_circumference = PV.HeadCircumference,
+NUll as Nutritional_status,
+format(cast(PIR.LMP as date),'yyyy-MM-dd') as Last_menstrual_period,
+CAST(PV.NursesComments AS varchar(MAX)) as Nurse_Comments,
+PV.DeleteFlag as Voided
+
+FROM [dbo].[PatientVitals] PV
+INNER JOIN Patient P ON P.Id = PV.PatientId
+INNER JOIN PatientMasterVisit PM ON PM.Id = PV.PatientMasterVisitId
+LEFT JOIN PregnancyIndicator PIR ON PIR.PatientMasterVisitId = PM.Id
+UNION
+SELECT
+Person_Id = P.PersonId,
+P.Id Patient_Id,
+Encounter_Date = format(cast(OV.VisitDate as date),'yyyy-MM-dd'),
+Encounter_ID = OV.Visit_Id,
+Visit_reason = NULL,
+Systolic_pressure = DPV.BPSystolic,
+Diastolic_pressure = DPV.BPDiastolic,
+Respiratory_rate = NULL,
+Pulse_rate = NULL,
+Oxygen_saturation = DPV.SP02,
+Weight = DPV.Weight,
+Height = DPV.Height,
+Temperature = DPV.Temp,
+BMI = NULL,
+Muac = DPV.Muac,
+Weight_for_age_zscore = NULL,
+Weight_for_height_zscore = NULL,
+BMI_Zscore = NULL,
+Head_circumference = NULL,
+NUll as Nutritional_status,
+format(cast(PCS.LMP as date),'yyyy-MM-dd') as Last_menstrual_period,
+NULL as Nurse_Comments,
+0 as Voided
+
+FROM dtl_PatientVitals DPV
+inner join Patient P on p.ptn_pk = DPV.Ptn_pk
+inner join ord_Visit OV ON OV.Visit_Id = DPV.Visit_pk
+inner join dtl_PatientClinicalStatus PCS ON PCS.Visit_pk = OV.Visit_Id;
+
+
+
+ 
