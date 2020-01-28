407ec969e52408471a6b750ca568680f724965e1
chesterosoronyas <31725146+chesterosoronyas@users.noreply.github.com>
2020-01-28 11:07:21 +0300

    changes on HTSTracing query from itemId to ItemName  under Reason_uncontacted

--- MSSQL_DML_Migration_Staging.sql
+++ MSSQL_DML_Migration_Staging.sql
@@ -420,7 +420,7 @@ Encounter_Date = T.DateTracingDone,
 Encounter_ID = T.Id,
 Contact_Type = (SELECT ItemName FROM LookupItemView WHERE ItemId=T.Mode AND MasterName = 'TracingMode'),
 Contact_Outcome = (SELECT ItemName FROM LookupItemView WHERE ItemId=T.Outcome AND MasterName = 'TracingOutcome'),
-Reason_uncontacted = (SELECT ItemId FROM LookupItemView WHERE ItemId= T.ReasonNotContacted AND MasterName in ('TracingReasonNotContactedPhone','TracingReasonNotContactedPhysical')),
+Reason_uncontacted = (SELECT ItemName FROM LookupItemView WHERE ItemId= T.ReasonNotContacted AND MasterName in ('TracingReasonNotContactedPhone','TracingReasonNotContactedPhysical')),
 T.OtherReasonSpecify,
 T.Remarks,
 T.DeleteFlag Voided
