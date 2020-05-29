31283a858edc33818b98797cd8d7cdebd8482505
chesterosoronyas <31725146+chesterosoronyas@users.noreply.github.com>
2020-05-29 11:11:12 +0300

    Adding extra scripts on complaint history,adverse drug reactionand drug allergies

---
+++ DML_4_Datasets/EXTRASCRIPTS/DML_Adverse_Drug_Reactions.sql
@@ -0,0 +1,16 @@
+
+select p.PersonId,pmv.VisitDate as EncounterDate,
+NULL as Encounter_ID,'Yes' as Has_adverse_drug_reaction,
+ae.EventCause as Medicine_causing_drug_reaction,
+ae.EventName as Drug_reaction,
+(select  top 1 [Name] from LookupItem where Id=Severity) as Drug_reaction_severity,
+
+NULL as Drug_reaction_onset_date,
+ae.[Action] as  Drug_reaction_action_taken,
+ae.DeleteFlag
+
+ from AdverseEvent ae
+inner join Patient p on p.Id =ae.PatientId
+inner join PatientMasterVisit pmv on pmv.Id=ae.PatientMasterVisitId
+
+
